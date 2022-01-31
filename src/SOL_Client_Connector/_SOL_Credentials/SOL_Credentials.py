319c11e0f43a44dc026c0723355be054cad1156f
DirectiveAthena <directive.athena@gmail.com>
2022-01-31 21:10:23 +0100

    Update SOL_Credentials.py

--- src/SOL_Client_Connector/_SOL_Credentials/SOL_Credentials.py
+++ src/SOL_Client_Connector/_SOL_Credentials/SOL_Credentials.py
@@ -13,7 +13,7 @@ from ..SOL_Encryption import pp_encrypt
 # - Code -
 # ----------------------------------------------------------------------------------------------------------------------
 class SOL_Credentials(BASE_SOL_Credentials):
-    def __init__(self, username:str,password:str,password_new:str=""):
+    def __init__(self, username:str,password:str,password_new:str=None):
         self._username = username
         self._password = password
         self._password_new = password_new
